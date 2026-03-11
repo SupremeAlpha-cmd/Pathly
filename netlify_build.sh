4437d743db7bf4f329579cd74a217a38b4062781
Oreoluwa Ifedinma Chiazor <chiazor.oreoluwa17@gmail.com>
2026-03-11 19:11:56 +0100

    fix netlify build and changed project name to Kirasa

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Install Flutter
-
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter
-
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Get dependencies
+# Generate .env file from Netlify environment variables
+echo "SUPABASE_URL=$SUPABASE_URL" > .env
+echo "SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY" >> .env
+echo "GEMINI_API_KEY=$GEMINI_API_KEY" >> .env
 
+# Get dependencies
 flutter pub get
 
 # Build web
-
 flutter build web --release
